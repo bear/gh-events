b3f901b23014bd1d386a6ed1d67158640a5b7aef
bear (Mike Taylor) <bear@andyet.net>
2014-08-11 21:25:29 -0400

    add uuid and timestamps

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var config = require('getconfig');
 var bucker = require('bucker').createLogger();
 var redis = require("redis");
+var uid = require('node-uuid');
+var moment = require('moment');
 var githubhook = require('githubhook');
 var client = redis.createClient(config.redis.port, config.redis.host);
 var github = githubhook({ port: config.github.port, logger: bucker });
@@ -13,6 +15,14 @@ github.listen();
 
 github.on('*', function (event, repo, ref, data) {
     bucker.log('sending ' + repo + ' ' + ref + ' event');
-    
-    client.publish('github', { 'repo': repo, 'ref': ref, 'data': data });
+
+    event = {
+        id: uuid.v1(),
+        type: 'github',
+        parent_id: '',
+        timestamp: moment.utc().unix() * 1000;,
+        payload: { 'repo': repo, 'ref': ref, 'data': data }
+    };
+
+    client.publish('github', JSON.stringify(event, null, 2));
 });
