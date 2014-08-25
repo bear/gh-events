a37bd49ec9966719f118b57560d6d150433e1489
bear (Mike Taylor) <bear@andyet.net>
2014-08-25 18:44:51 -0400

    add webhook config, code to call webhook and wrap redis in a redis-config check

--- server.js
+++ server.js
@@ -4,16 +4,23 @@ var redis = require("redis");
 var uuid = require('uuid');
 var moment = require('moment');
 var githubhook = require('githubhook');
-var client = redis.createClient(config.redis.port, config.redis.host);
 var github = githubhook({ port: config.github.port, logger: bucker });
 
-client.on('error', function (err) {
-    bucker.log('redis error ' + err);
-});
+if config.redis {
+    var client = redis.createClient(config.redis.port, config.redis.host);
+
+    client.on('error', function (err) {
+        bucker.log('redis error ' + err);
+    });
+};
+
+if config.webhook {
+    var wreck = require('wreck');
+};
 
 github.listen();
 
-github.on('*', function (event, repo, ref, data) {
+github.on('*', function cbEvent(event, repo, ref, data) {
     bucker.log('sending ' + repo + ' ' + ref + ' event');
 
     eventData = {
@@ -24,5 +31,19 @@ github.on('*', function (event, repo, ref, data) {
         payload: { 'event': event, 'repo': repo, 'ref': ref, 'data': data }
     };
 
-    client.publish('github', JSON.stringify(eventData, null, 2));
+    if config.redis {
+        client.publish('github', JSON.stringify(eventData, null, 2));
+    };
+
+    if config.wreck {
+        wreck.post(config.webhook.uri, options={'payload': JSON.stringify(eventData, null, 2)},
+            function cbWreck(error, resp, payload) {
+                if err {
+                    bucker.error(err);
+                } else {
+                    bucker.log('webhook called');
+                };
+            };
+        );
+    };
 });
